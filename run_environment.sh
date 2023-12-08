c2a707531d7c4bfdb0dda62c92be75330137c2c0
Lyla Kiratiwudhikul <lkiratiwudhikul@fas.harvard.edu>
2023-12-08 11:10:09 -0500

    added sklearn and pytorch to requirements

--- run_environment.sh
+++ run_environment.sh
@@ -1,3 +1,4 @@
 pip install git+https://github.com/indrasweb/expert-offline-rl.git
 pip install -r requirement.txt
 pip install ncps "ale-py==0.7.4" "ray[rllib]==2.1.0" "gym[atari,accept-rom-license]==0.23.1"
+conda install -c pytorch -y pytorch
