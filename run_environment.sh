ccc6cc3d698ef8a60afa82d32eb6a0dd1f4a9e16
Lyla Kiratiwudhikul <lkiratiwudhikul@fas.harvard.edu>
2023-12-08 11:03:44 -0500

    updated env requirement for visualization

--- run_environment.sh
+++ run_environment.sh
@@ -1,2 +1,3 @@
 pip install git+https://github.com/indrasweb/expert-offline-rl.git
-pip install requirement.txt
+pip install -r requirement.txt
+pip install ncps "ale-py==0.7.4" "ray[rllib]==2.1.0" "gym[atari,accept-rom-license]==0.23.1"
